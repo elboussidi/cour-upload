a4bf4e7b3ead0fb8d3855623073c67874270653a
abdelmajid <abdelmajidboussidi@gmail.com>
2020-04-18 13:38:41 +0000

    frist page

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+ <html>
+    <head>
+        <meta charset="UTF-8">
+        
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+         <link rel="stylesheet" href="css/all.min.css">
+         <link rel="stylesheet" href="bootstrap/css/style.css">
+      <style type="text/css">
+           @font-face{
+             font-family: tdt ;
+            src: url('fo.ttf');
+           }
+            @font-face{
+             font-family: tst ;
+            src: url('mp.ttf');
+           }
+            p{
+               font-family: tst ;
+           }     
+     </style>
+     
+    <body>
+        
+   
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded  ">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="btn btn-success" href="#">LOG IN</a>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">admin_page</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          module
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">berutique</a>
+          <a class="dropdown-item" href="#">sage</a>
+          <a class="dropdown-item" href="#">travaux fin exrice</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>     
+       <!-- --------------------  fin header  ---------------- --> 
+       <br>
+       
+       <div class="jumbotron">
+  <h1 class="display-3">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+       
+       <!----------fin cont  ----------------------> 
+      <br><hr>
+       <?php include './footer.php'; ?>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    
+    <script src="bootstrap/js/bootstrap.min.js">  </script>
+    <script src="bootstrap/js/jquery-3.3.1.min.js">  </script>
+</html>
+
+  
